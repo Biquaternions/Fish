29926e63cc07f73f867dd0112ed68e04f5c45cf7
Biquaternions <biquaternions@serlith.net>
2025-07-28 00:16:56 -0500

    Updated config

--- fish-server/src/main/java/net/serlith/fish/FishConfig.java
+++ fish-server/src/main/java/net/serlith/fish/FishConfig.java
@@ -4,6 +4,17 @@ import net.j4c0b3y.api.config.ConfigHandler;
 import net.j4c0b3y.api.config.StaticConfig;
 import java.io.File;
 
+@StaticConfig.Header({
+    "",
+    "Fish Configuration \uD83D\uDC1F",
+    "This project is a meme/playground, here be dragons~",
+    "",
+    "Configurations marked with:",
+    " \uD83D\uDD25 Support hot reload with /fish reload",
+    " \uD83D\uDD03 Require a server restart to apply",
+    " ⚠️ Preferably use a clean new world unless you know how to properly adapt your current one",
+    ""
+})
 public class FishConfig extends StaticConfig {
 
     @Ignore
@@ -28,7 +39,8 @@ public class FishConfig extends StaticConfig {
     public static class ASYNC {
 
         @Comment({
-            "Ticks each world/level in a separate thread",
+            "\uD83D\uDD03 Ticks each world/level in a separate thread",
+            "Regardless of the name given here, it's actually Parallel World Ticking rather than fully async",
             "Worlds do not tick independently, they have to wait until all worlds are done with the current tick to continue"
         })
         public static class WORLD_TICKING {
@@ -36,6 +48,11 @@ public class FishConfig extends StaticConfig {
             @Ignore
             public static boolean _ENABLED = true;
 
+            @Comment({
+                "Each world will have its own thread, which means 20 worlds = 20 threads",
+                "This option refers to how many worlds (threads) can work at the same time, to not stress the CPU",
+                "If this value is lower than the amount of worlds, the remaining worlds will wait until others are done"
+            })
             public static int THREADS = 3;
             @Ignore
             public static int _THREADS = 3;
