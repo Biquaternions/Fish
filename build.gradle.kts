0553ab81c99348d3447f9f83089e0d218b0c2a06
Biquaternions <biquaternions@serlith.net>
2025-12-11 13:58:50 -0500

    Update to 1.21.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java // TODO java launcher tasks
-    id("io.papermc.paperweight.patcher") version "2.0.0-beta.17"
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.19"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
@@ -76,7 +76,6 @@ subprojects {
         mavenLocal()
         maven(paperMavenPublicUrl)
         maven("https://jitpack.io")
-        maven("https://repo.j4c0b3y.net/public/")
     }
 }
 
