ce4afec7417bf01ede46b39ffba5c1fc4d0354d2
Biquaternions <biquaternions@serlith.net>
2026-07-21 20:59:17 -0500

    World tick events

---
+++ fish-api/src/main/java/me/biquaternions/fish/event/world/WorldTickEndEvent.java
@@ -0,0 +1,62 @@
+package me.biquaternions.fish.event.world;
+
+import org.bukkit.World;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.world.WorldEvent;
+import org.jetbrains.annotations.ApiStatus;
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+public class WorldTickEndEvent extends WorldEvent {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+
+    private final int tickNumber;
+    private final double tickDuration;
+    private final long timeEnd;
+
+    @ApiStatus.Internal
+    public WorldTickEndEvent(final World world, final int tickNumber, final double tickDuration, final long timeRemaining) {
+        super(world, false);
+        this.tickNumber = tickNumber;
+        this.tickDuration = tickDuration;
+        this.timeEnd = System.nanoTime() + timeRemaining;
+    }
+
+    /**
+     * @return What tick this was since start (first tick = 1)
+     */
+    public int getTickNumber() {
+        return this.tickNumber;
+    }
+
+    /**
+     * @return Time in milliseconds of how long this tick took
+     */
+    public double getTickDuration() {
+        return this.tickDuration;
+    }
+
+    /**
+     * Amount of nanoseconds remaining before the next tick should start.
+     * <p>
+     * If this value is negative, then that means the server has exceeded the tick time limit and TPS has been lost.
+     * <p>
+     * Method will continuously return the updated time remaining value. (return value is not static)
+     *
+     * @return Amount of nanoseconds remaining before the next tick should start
+     */
+    public long getTimeRemaining() {
+        return this.timeEnd - System.nanoTime();
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+}
